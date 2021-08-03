cf9ad6912c2ad0edabadb084fc891f26916ce152
Nil Quera <nlquera97@gmail.com>
2021-08-04 01:38:37 +0200

    Base code and add author

---
+++ frontend/src/pages/AuthorList/AuthorList.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
+
+import { ROUTE_AUTHOR_PREFIX, ROUTE_AUTHOR_CREATE } from '../../constants';
+import { listAuthors } from '../../services/authors';
+
+function AuthorList() {
+    const [authors, setAuthors] = useState([]);
+
+    useEffect(() => {
+        const fetchAuthors = async () => {
+            const data = await listAuthors();
+            setAuthors(data);
+        };
+
+        fetchAuthors();
+    }, []);
+
+    const renderAuthors = () => authors.map((author) => {
+        const { id, firstName, lastName } = author;
+
+        return (
+            <tr key={ id }>
+                <td>
+                    <Link to={ `${ROUTE_AUTHOR_PREFIX}/${id}` }>{lastName}, { firstName }</Link>
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <div className="ArticleList">
+            <h1>Authors</h1>
+            <Link className="d-block mb-3" to={ ROUTE_AUTHOR_CREATE }>
+                Create a new Author
+            </Link>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { renderAuthors() }
+                </tbody>
+            </Table>
+        </div>
+    );
+}
+
+export default AuthorList;
